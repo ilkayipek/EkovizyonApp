097b0ea17db8b721f255449a367b483dcb228346
ilkay-ipek <ilkayy.ipek@gmail.com>
2024-04-04 14:03:17 +0300

    Initial commit

---
+++ SurdurulebilirMahalleler/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SurdurulebilirMahalleler
+//
+//  Created by MacBook on 1.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
