57ed4f99a09d6ee22819a510709438d18575cce2
ilkay-ipek <ilkayy.ipek@gmail.com>
2024-04-16 01:12:28 +0300

    Create FirebaseCollections.swift
    for in Firebase  collections

---
+++ SurdurulebilirMahalleler/Enum/FirebaseCollections.swift
@@ -0,0 +1,22 @@
+//
+//  FirebaseCollections.swift
+//  SurdurulebilirMahalleler
+//
+//  Created by MacBook on 15.04.2024.
+//
+
+import Foundation
+
+enum FirebaseCollections: String {
+    case comments = "Comments"
+    case products = "Products"
+    case posts = "Posts"
+    case follows = "Follows"
+    case userProducts = "UserProducts"
+    case users = "Users"
+    case userDetail = "UserDetail"
+    case eventParticipants = "EventParticipants"
+    case locations = "Locations"
+    case events = "Events"
+    case pointDetails = "PointDetails"
+}
