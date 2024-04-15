4e9dd959a04d3ec19bf0e46a479b827343479693
ilkay-ipek <ilkayy.ipek@gmail.com>
2024-04-16 01:11:44 +0300

    Create DocumentError.swift
    for Network Manager

---
+++ SurdurulebilirMahalleler/Enum/DocumentError.swift
@@ -0,0 +1,12 @@
+//
+//  DocumentError.swift
+//  SurdurulebilirMahalleler
+//
+//  Created by MacBook on 15.04.2024.
+//
+
+import Foundation
+
+enum DocumentError: Error {
+    case documentNotFound
+}
