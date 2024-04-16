1bd54e14404fc6624fb4f3f86e89632d157bd41f
ilkay-ipek <ilkayy.ipek@gmail.com>
2024-04-16 13:51:45 +0300

    Create BaseViewModel.swift
    animation closures defined

---
+++ SurdurulebilirMahalleler/Scenes/Base/BaseViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  BaseViewModel.swift
+//  SurdurulebilirMahalleler
+//
+//  Created by MacBook on 15.04.2024.
+//
+
+import Foundation
+
+class BaseViewModel {
+    var loadingAnimationStart: ((String) -> Void)?
+    var loadingAnimationStop: (() -> Void)?
+    var successAnimation: ((String) -> Void)?
+    var failAnimation: ((String) -> Void)?
+    var gradientLoagingTabAnimation: CustomGradientLoadingAnimation?
+}
