92425f8709e8e7b51725ffff1771b4a5b65882a1
ilkay-ipek <ilkayy.ipek@gmail.com>
2024-04-22 12:26:20 +0300

    Update SignUpViewModel.swift
    SignIn With Google func added

--- SurdurulebilirMahalleler/Scenes/SignUp/SignUpViewModel.swift
+++ SurdurulebilirMahalleler/Scenes/SignUp/SignUpViewModel.swift
@@ -6,7 +6,22 @@
 //
 
 import Foundation
+import UIKit.UIViewController
 
 class SignUpViewModel: BaseViewModel {
     
+    func signUpWithGoogle(controller: UIViewController ,_ closure: @escaping (Bool) -> Void) {
+        loadingAnimationStart?("please wait..")
+        AuthManager.shared.withGoogleSession(controller: controller) { [weak self] (status, error) in
+            guard let self else {return}
+            if let error {
+                self.failAnimation?("something went wrong\nerror Message: \(error.localizedDescription)")
+                closure(false)
+            } else if status {
+                self.loadingAnimationStop?()
+                closure(true)
+            }
+            self.gradientLoagingTabAnimation?.stopAnimations()
+        }
+    }
 }
