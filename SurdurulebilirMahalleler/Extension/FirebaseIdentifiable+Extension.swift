112ce2ffe2097c0a02739c56abdb8f9f91cce805
ilkay-ipek <ilkayy.ipek@gmail.com>
2024-04-22 12:12:57 +0300

    Update FirebaseIdentifiable+Extension.swift
    FirebaseCollection enum parameter changed

--- SurdurulebilirMahalleler/Extension/FirebaseIdentifiable+Extension.swift
+++ SurdurulebilirMahalleler/Extension/FirebaseIdentifiable+Extension.swift
@@ -9,17 +9,17 @@ import Foundation
 
 extension FirebaseIdentifiable {
     /// POST to Firebase
-    func post(to collection: FirebaseCollections.RawValue,_ completion: @escaping (Result<Self, Error>) -> Void){
-        return Network.shared.post(self, to: collection, completion: completion)
+    func post(to collection: FirebaseCollections,_ completion: @escaping (Result<Self, Error>) -> Void){
+        return Network.shared.post(self, to: collection.rawValue, completion: completion)
     }
 
     /// PUT to Firebase
-    func put(to collection: FirebaseCollections.RawValue, _ completion: @escaping (Result<Self, Error>)-> Void) {
-        return Network.shared.put(self, to: collection, completion: completion)
+    func put(to collection: FirebaseCollections, _ completion: @escaping (Result<Self, Error>)-> Void) {
+        return Network.shared.put(self, to: collection.rawValue, completion: completion)
     }
 
     /// DELETE from Firebase
-    func delete(from collection: FirebaseCollections.RawValue, _ completion: @escaping (Result<Void, Error>)-> Void) {
-        return Network.shared.delete(self, in: collection, completion: completion)
+    func delete(from collection: FirebaseCollections, _ completion: @escaping (Result<Void, Error>)-> Void) {
+        return Network.shared.delete(self, in: collection.rawValue, completion: completion)
     }
 }
