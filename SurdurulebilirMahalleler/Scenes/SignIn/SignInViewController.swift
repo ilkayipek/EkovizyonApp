197599c87251add56262e9a35195d3c6ef984295
ilkay-ipek <ilkayy.ipek@gmail.com>
2024-04-23 13:04:13 +0300

    SignIn MVVM Created

---
+++ SurdurulebilirMahalleler/Scenes/SignIn/SignInViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SignInViewController.swift
+//  SurdurulebilirMahalleler
+//
+//  Created by MacBook on 23.04.2024.
+//
+
+import UIKit
+
+class SignInViewController: BaseViewController<SignInViewModel> {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        viewModel = SignInViewModel()
+        
+    }
+
+}
---
+++ SurdurulebilirMahalleler/Scenes/SignIn/SignInViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  SignInViewModel.swift
+//  SurdurulebilirMahalleler
+//
+//  Created by MacBook on 23.04.2024.
+//
+
+import Foundation
+
+class SignInViewModel: BaseViewModel {
+    
+}
