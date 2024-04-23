3e49597ff0accbbe890f2a184826e5f4b0043110
ilkay-ipek <ilkayy.ipek@gmail.com>
2024-04-23 17:54:59 +0300

    Update SignInViewModel.swift
    signInWithEmail func added.
    signInWithGoogle func added.

--- SurdurulebilirMahalleler/Scenes/SignIn/SignInViewModel.swift
+++ SurdurulebilirMahalleler/Scenes/SignIn/SignInViewModel.swift
@@ -6,7 +6,41 @@
 //
 
 import Foundation
+import UIKit.UIViewController
 
 class SignInViewModel: BaseViewModel {
-    
+    func signInWithEmail(_ email: String, _ password: String, _ closure: @escaping (Bool) -> Void) {
+        loadingAnimationStart?("")
+        AuthManager.shared.signInWithEmail(email, password) {[weak self] status, error in
+            guard let self else {return}
+            
+            if let error {
+                self.failAnimation?("Hata: \(error.localizedDescription)")
+                closure(false)
+            } else if status {
+                closure(true)
+                self.loadingAnimationStop?()
+            } else {
+                self.failAnimation?("Bilinmeyen Bir hata Oluştu")
+                closure(false)
+            }
+        }
+    }
+
+    func signInWithGoogle(controller: UIViewController, _ closure: @escaping (Bool) -> Void) {
+        loadingAnimationStart?("")
+        
+        AuthManager.shared.withGoogleSession(controller: controller) { [weak self] status, error in
+            guard let self else {return}
+            
+            if let error {
+                self.failAnimation?("Hata: \(error.localizedDescription)")
+                closure(false)
+            } else if status{
+                closure(true)
+            } else {
+                self.failAnimation?("Bilinmeyen bir hata oluştu")
+            }
+        }
+    }
 }
