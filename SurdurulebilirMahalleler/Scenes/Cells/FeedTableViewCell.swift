a70171ad913737ea1ae06ff2cefe65df9537ecc1
ilkay-ipek <ilkayy.ipek@gmail.com>
2024-04-24 22:23:00 +0300

    Update FeedTableViewCell.swift
    features defined.
    features connected with code.
    cell load with loadCell func.

--- SurdurulebilirMahalleler/Scenes/Cells/FeedTableViewCell.swift
+++ SurdurulebilirMahalleler/Scenes/Cells/FeedTableViewCell.swift
@@ -8,16 +8,39 @@
 import UIKit
 
 class FeedTableViewCell: UITableViewCell {
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var contentLabel: UILabel!
+    @IBOutlet weak var profileImageView: CustomUIImageView!
+    @IBOutlet weak var contentImageView: UIImageView!
+    @IBOutlet weak var likeButton: UIButton!
+    @IBOutlet weak var commentsButton: UIButton!
 
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
-        // Configure the view for the selected state
+    }
+    
+    func loadCell(_ postModel: PostModel,_ userModel: UserModel) {
+        nameLabel.text = userModel.name
+        userNameLabel.text = "@\(userModel.username ?? "")"
+        contentLabel.text = postModel.contentText
+        likeButton.setTitle("\(postModel.totalLike)", for: .normal)
+        commentsButton.setTitle("\(postModel.totalComment)", for: .normal)
+       
+        if let pofileImageUrl = URL(string: userModel.profileUrl ?? "") {
+            profileImageView.loadImage(url: pofileImageUrl, placeHolderImage: nil, nil)
+        }
+        if let postContentUrl = URL(string: postModel.contentImageUrl ?? "") {
+            contentImageView.isHidden = false
+            contentImageView.loadImage(url: postContentUrl, placeHolderImage: nil, nil)
+        } else {
+            contentImageView.isHidden = true
+        }
     }
     
 }
