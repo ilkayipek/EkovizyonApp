28bb2dc3b414be90cacac80b91c1b99bf3544fef
ilkay-ipek <ilkayy.ipek@gmail.com>
2024-04-24 22:20:21 +0300

    Update FeedViewModel.swift
    posts get according to timestamp with getPosts.
    The user belonging to each post is taken with  getPostUser func.

--- SurdurulebilirMahalleler/Scenes/Feed/FeedViewModel.swift
+++ SurdurulebilirMahalleler/Scenes/Feed/FeedViewModel.swift
@@ -7,6 +7,67 @@
 
 import Foundation
 
-
 class FeedViewModel: BaseViewModel {
+    
+    func getPosts(_ closure: @escaping ([PostModel]?,[UserModel]?) -> Void ) {
+        
+        let ref = Network.shared.database.collection(FirebaseCollections.posts.rawValue)
+        let query = ref.order(by: "timestamp", descending: true)
+        var users = [UserModel]()
+        
+        gradientLoagingTabAnimation?.startAnimations()
+        Network.shared.getMany(of: PostModel.self, with: query) { [weak self] (result: Result<[PostModel], Error>) in
+            guard let self else {return}
+            
+            switch result {
+            case .success(let posts):
+                
+                for post in posts {
+                    self.getPostUser(post.userReference?.documentID ?? " ") { user in
+                        guard let user else {return}
+                        users.append(user)
+                        closure(posts,users)
+                    }
+                }
+                
+            case .failure(let error):
+                closure(nil,nil)
+                print("Hata: \(error)")
+                self.failAnimation?("Hata: \(error.localizedDescription)")
+            }
+            self.gradientLoagingTabAnimation?.stopAnimations()
+        }
+    }
+    
+    func getPostUser(_ userId: String, _ closure: @escaping (UserModel?) ->Void) {
+        let ref = Network.shared.refCreate(collection: .users, uid: userId)
+        
+        Network.shared.getDocument(reference: ref) { (result: Result<UserModel?, any Error>) in
+            switch result {
+            case .success(let data):
+                if let data {
+                    closure(data)
+                    print(data)
+                } else {
+                    closure(nil)
+                }
+                
+            case .failure(let error):
+                closure(nil)
+                print(error)
+            }
+        }
+    }
+    
+    func isItLikedPost(postId: String, _ closure: @escaping (Bool) -> Void) {
+        
+    }
+    
+    func sendLike(postId: String, userId: String, _ closure: @escaping (Bool) -> Void) {
+        
+    }
+    
+    func updatePost(_ postModel: PostModel) {
+        
+    }
 }
