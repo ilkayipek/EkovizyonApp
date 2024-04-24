427395ab4209af4f6bd4a4c14663289873c0166e
ilkay-ipek <ilkayy.ipek@gmail.com>
2024-04-24 23:49:51 +0300

    Create FireBaseStorageFolders.swift

---
+++ SurdurulebilirMahalleler/Enum/FireBaseStorageFolders.swift
@@ -0,0 +1,15 @@
+//
+//  FireBaseStorageFolders.swift
+//  SurdurulebilirMahalleler
+//
+//  Created by MacBook on 24.04.2024.
+//
+
+import Foundation
+
+enum FireBaseStorageFolders: String {
+    case postImages
+    case profileImages
+    case eventImages
+    case productImages
+}
