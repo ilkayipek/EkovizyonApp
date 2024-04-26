6122855a5e78717aee767497dca1aff18ec4901b
ilkay-ipek <ilkayy.ipek@gmail.com>
2024-04-26 16:06:30 +0300

    Update FirebaseIdentifiable+Extension.swift
    raw value was deleted from post, put, delete functions collection parameters

--- SurdurulebilirMahalleler/Extension/FirebaseIdentifiable+Extension.swift
+++ SurdurulebilirMahalleler/Extension/FirebaseIdentifiable+Extension.swift
@@ -10,16 +10,16 @@ import Foundation
 extension FirebaseIdentifiable {
     /// POST to Firebase
     func post(to collection: FirebaseCollections,_ completion: @escaping (Result<Self, Error>) -> Void){
-        return Network.shared.post(self, to: collection.rawValue, completion: completion)
+        return Network.shared.post(self, to: collection, completion: completion)
     }
 
     /// PUT to Firebase
     func put(to collection: FirebaseCollections, _ completion: @escaping (Result<Self, Error>)-> Void) {
-        return Network.shared.put(self, to: collection.rawValue, completion: completion)
+        return Network.shared.put(self, to: collection, completion: completion)
     }
 
     /// DELETE from Firebase
     func delete(from collection: FirebaseCollections, _ completion: @escaping (Result<Void, Error>)-> Void) {
-        return Network.shared.delete(self, in: collection.rawValue, completion: completion)
+        return Network.shared.delete(self, in: collection, completion: completion)
     }
 }
