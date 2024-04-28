da1581c40116a1e490d646183dc496cf3df0b949
ilkay-ipek <ilkayy.ipek@gmail.com>
2024-04-28 12:51:27 +0300

    Events MVVM created

---
+++ SurdurulebilirMahalleler/Scenes/Events/EventsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  EventsViewController.swift
+//  SurdurulebilirMahalleler
+//
+//  Created by MacBook on 28.04.2024.
+//
+
+import UIKit
+
+class EventsViewController: BaseViewController<EventsViewModel> {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        viewModel = EventsViewModel()
+        
+    }
+
+}
---
+++ SurdurulebilirMahalleler/Scenes/Events/EventsViewModel.swift
@@ -0,0 +1,10 @@
+//
+//  EventsViewModel.swift
+//  SurdurulebilirMahalleler
+//
+//  Created by MacBook on 28.04.2024.
+//
+
+class EventsViewModel: BaseViewModel {
+    
+}
