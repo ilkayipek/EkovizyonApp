1051add975978f5eb104b42de50b8e3e04ff455f
ilkay-ipek <ilkayy.ipek@gmail.com>
2024-05-01 13:17:37 +0300

    Update PointDetails.swift
    PointModel fix.
    availablePoint feature added.
    remove CodingKeys

--- SurdurulebilirMahalleler/Models/PointDetails.swift
+++ SurdurulebilirMahalleler/Models/PointDetails.swift
@@ -15,19 +15,9 @@ struct PointDetail: FirebaseIdentifiable {
     var monthlyScore: Int
     var totalEventPoint: Int
     var totalLocationPoint: Int
+    var availablePoint: Int
     var userRef: DocumentReference?
     var userModel: UserModel?
-    
-    enum CodingKeys: String, CodingKey {
-        case id
-        case totalScore
-        case weeklyScore
-        case monthlyScore
-        case totalEventPoint
-        case totalLocationPoint
-        case userRef
-        case userModel
-    }
     
     init(
         id: String = UUID().uuidString,
@@ -36,6 +26,7 @@ struct PointDetail: FirebaseIdentifiable {
         monthlyScore: Int = 0,
         totalEventPoint: Int = 0,
         totalLocationPoint: Int = 0,
+        availablePoint:Int = 0,
         userRef: DocumentReference? = nil,
         userModel: UserModel? = nil
     ) {
@@ -45,6 +36,7 @@ struct PointDetail: FirebaseIdentifiable {
         self.monthlyScore = monthlyScore
         self.totalEventPoint = totalEventPoint
         self.totalLocationPoint = totalLocationPoint
+        self.availablePoint = availablePoint
         self.userRef = userRef
         self.userModel = userModel
     }
