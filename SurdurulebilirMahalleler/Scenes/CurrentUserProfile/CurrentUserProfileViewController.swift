b0d490e736ad9a1177851f2e901e4ba210c7a343
ilkay-ipek <ilkayy.ipek@gmail.com>
2024-05-04 18:03:56 +0300

    CurrentUserProfile MVVM created

---
+++ SurdurulebilirMahalleler/Scenes/CurrentUserProfile/CurrentUserProfileViewController.swift
@@ -0,0 +1,18 @@
+//
+//  CurrentUserProfileViewController.swift
+//  SurdurulebilirMahalleler
+//
+//  Created by MacBook on 4.05.2024.
+//
+
+import UIKit
+
+class CurrentUserProfileViewController: BaseViewController<CurrentUserProfileViewModel> {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        viewModel = CurrentUserProfileViewModel()
+        
+    }
+
+}
---
+++ SurdurulebilirMahalleler/Scenes/CurrentUserProfile/CurrentUserProfileViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  CurrentUserProfileViewModel.swift
+//  SurdurulebilirMahalleler
+//
+//  Created by MacBook on 4.05.2024.
+//
+
+import Foundation
+
+class CurrentUserProfileViewModel: BaseViewModel {
+    
+}
