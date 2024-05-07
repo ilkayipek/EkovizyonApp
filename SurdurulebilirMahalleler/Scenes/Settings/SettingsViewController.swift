62099521d7bf6e19e63af08e8b64d0b9c1a353ea
ilkay-ipek <ilkayy.ipek@gmail.com>
2024-05-07 19:48:52 +0300

    Settings MVVM created

---
+++ SurdurulebilirMahalleler/Scenes/Settings/SettingsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsViewController.swift
+//  SurdurulebilirMahalleler
+//
+//  Created by MacBook on 7.05.2024.
+//
+
+import UIKit
+
+class SettingsViewController: BaseViewController<SettingsViewModel> {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        viewModel = SettingsViewModel()
+        
+    }
+
+}
---
+++ SurdurulebilirMahalleler/Scenes/Settings/SettingsViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  SettingsViewModel.swift
+//  SurdurulebilirMahalleler
+//
+//  Created by MacBook on 7.05.2024.
+//
+
+import Foundation
+
+class SettingsViewModel: BaseViewModel {
+    
+}
